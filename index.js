1323cfbe67eaea2d016c9f9ca62f5a95211388e2
Hiroki Osame <hiroki@weebly.com>
2019-12-10 13:54:08 -0500

    Added option to pass in dimension

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
+const assert = require('assert');
 const { promisify } = require('util');
 const sharp = require('sharp');
 const potrace = require('potrace');
 const cheerio = require('cheerio');
+const loaderUtils = require('loader-utils');
 
-function getSvgDimensions(svg) {
-	const $ = cheerio.load(svg, { xmlMode: true });
-	const $svg = $('svg');
-	return {
-		width: parseInt($svg.attr('width'), 10),
-		height: parseInt($svg.attr('height'), 10),
-	};
+const dimensions = (width = 0, height = 0) => ({
+	width: parseInt(width, 10),
+	height: parseInt(height, 10),
+});
+
+function getSvgDimensions($svg) {
+	let d = dimensions($svg.attr('width'), $svg.attr('height'));
+
+	if (d.width && d.height) { return d; }
+
+	const viewBox = $svg.attr('viewBox');
+	if (viewBox) {
+		return dimensions(...viewBox.split(' ').slice(2));
+	}
 }
 
 const trace = promisify(potrace.trace);
-
 module.exports = function(content, map, meta) {
 	const callback = this.async();
 
 	(async () => {
+		const options = loaderUtils.getOptions(this) || {};
 		let svg = content;
+		const $ = cheerio.load(svg, { xmlMode: true });
+		const $svg = $('svg');
+		const dimensions = options.dimensions || getSvgDimensions($svg);
 
-		try {
-			const buf = await sharp(Buffer.from(svg), { density: 2400 }).toBuffer();
-			svg = await trace(buf, getSvgDimensions(svg));			
-		} catch (e) {
-			return callback(e);
-		}
+		assert(dimensions.width, 'SVG missing width');
+		assert(dimensions.height, 'SVG missing height');
 
-		callback(null, svg);
-	})();
+		// The dimensions here are used by sharp to render it as a PNG
+		// Since we're using a high rendering density, a high SVG dimension creates too big of a PNG and crashes
+		$svg.attr({ width: 20, height: 20 });
+
+		svg = $.html();
+
+		const buf = await sharp(Buffer.from(svg), { density: 2400 }).toBuffer();
+		svg = await trace(buf, dimensions);
+
+		return svg;
+	})().then(
+		svg => callback(null, svg),
+		err => callback(err),
+	);
 };
